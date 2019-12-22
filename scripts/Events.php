d180cdf79a9d7077443647a9ef247eb17e3f0bb0
eslamradi <eslamradii@gmail.com>
2019-12-22 22:01:06 +0200

    add post install or update handler for config file

---
+++ scripts/Events.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Iradi\Scripts;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class Events
+{
+    public static function publishConfigs(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        $homeDir = $event->getComposer()->getConfig()->get('home');
+
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $files = glob($vendorDir, '/*iradi-settings/*');
+
+        foreach ($files as $file) {
+            $destPath = $homeDir.'/iradi-settings/'.basename($file);
+            if(!file_exists($destPath)){
+                copy($file, $destPath);
+                print("Published File: {basename($file)}");
+            } else {
+                print("File already exists: {basename($file)}");
+            }
+        }
+    }
+}
